0d9706b6284a787af8c1831559a8a39a51c342f7
YJY <kdjmgpf9jb@126.com>
2020-01-06 21:11:26 +0800

    完成首页上部分

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>
-    <h1>首页</h1>
+  <div id="home">
+    <nav-bar id="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners='banners'/>
+    <recommend-view :recommends='recommends'/>
+    <FeatureView/>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComp/HomeSwiper'
+import RecommendView from './childComp/RecommendView'
+import FeatureView from './childComp/FeatureView'
+
+import {getHomeMultidata} from '../../network/home'
+
    export default {
-   name:'home'
+   name:'home',
+   components: {
+     NavBar,
+     RecommendView,
+     FeatureView,
+     HomeSwiper
+   },
+   data() {
+     return {
+       banners: [],
+       recommends: []
+     }
+   },
+   created() {
+     getHomeMultidata().then(res => {
+       this.banners = res.data.banner.list
+       this.recommends = res.data.recommend.list
+     })
+   }
 }
 </script>
 
 <style scoped>
-
+#home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
---
+++ src/views/home/childComp/RecommendView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="recommend">
+      <div class="recommend-item" v-for="item in recommends" :key="item.id">
+          <a :href="item.link">
+              <img :src="item.image" alt="">
+              <div class="title">{{item.title}}</div>
+          </a>
+      </div>
+  </div>
+</template>
+
+<script>
+   export default {
+   name:'RecommendView',
+   props: {
+       recommends: {
+           type: Array,
+           default() {
+               return []
+           }
+       }
+   }
+}
+</script>
+
+<style scoped>
+.recommend{
+    display: flex;
+    text-align: center;
+    margin-top: 10px;
+
+    border-bottom: 10px solid #eee
+}
+.recommend-item {
+    flex: 1;
+    margin-bottom: 25px;
+}
+.recommend-item img {
+    width: 65px;
+    height: 65px;
+    margin-bottom: 10px;
+}
+.title {
+    font-size: 14px;
+}
+
+</style>
